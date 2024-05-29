0b2799f5a2cc3c4c5d9f38599f0295482cce2d6d
htamagnus <agathamagnusg@hotmail.com>
2024-05-29 18:55:21 -0300

    add env port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(3003);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
